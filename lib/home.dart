df13313543d5a71305870ceab943e775cb1f7975
Mohamed Ali <elm.10203040@gmail.com>
2024-07-16 00:21:22 +0300

    appbar

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const String routeName='Home';
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Islami'),
+
+
+      ),
+    );
+  }
+}
